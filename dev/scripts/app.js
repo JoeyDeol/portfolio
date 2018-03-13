28aab57ec1c5c8778db14b5507ca4375a3273a7c
Joey Deol <gurjothdeol@gmail.com>
2018-03-13 11:23:39 -0400

    Added in Navigation bar, About Me Layout Complete, Skills Layout Complete, Currently working on Contact Section.

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,6 +1,67 @@
 const portfolio = {}; 
 
+portfolio.smoothScroll = function() {
+    new SmoothScroll('a[href*="#"]', {
+        // Selectors
+        ignore: "[data-scroll-ignore]", // Selector for links to ignore (must be a valid CSS selector)
+        header: null, // Selector for fixed headers (must be a valid CSS selector)
+        // Speed & Easing
+        speed: 500, // Integer. How fast to complete the scroll in milliseconds
+        offset: 150 // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
+    });
+}
+
+portfolio.bannerScroll = function () {
+    // Cache selectors
+    var lastId,
+        topMenu = $("#top-menu"),
+        topMenuHeight = topMenu.outerHeight() + 15,
+        // All list items
+        menuItems = topMenu.find("a"),
+        // Anchors corresponding to menu items
+        scrollItems = menuItems.map(function () {
+            var item = $($(this).attr("href"));
+            if (item.length) { return item; }
+        });
+    
+    // Bind click handler to menu items
+    // so we can get a fancy scroll animation
+    menuItems.click(function (e) {
+        var href = $(this).attr("href"),
+            offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+        $('html, body').stop().animate({
+            scrollTop: offsetTop
+        }, 300);
+        e.preventDefault();
+    });
+    
+    // Bind to scroll
+    $(window).scroll(function () {
+        // Get container scroll position
+        var fromTop = $(this).scrollTop() + topMenuHeight;
+    
+        // Get id of current scroll item
+        var cur = scrollItems.map(function () {
+            if ($(this).offset().top < fromTop)
+                return this;
+        });
+        // Get the id of the current element
+        cur = cur[cur.length - 1];
+        var id = cur && cur.length ? cur[0].id : "";
+    
+        if (lastId !== id) {
+            lastId = id;
+            // Set/remove active class
+            menuItems
+                .parent().removeClass("active")
+                .end().filter("[href='#" + id + "']").parent().addClass("active");
+        }
+    });
+}
+
 portfolio.init = function() {
+    // portfolio.smoothScroll();
+    portfolio.bannerScroll();
 };
 
 $(function() {
